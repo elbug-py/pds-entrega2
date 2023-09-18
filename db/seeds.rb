e899967dd319d30dfb1376b09c713f7a497a007e
webtech <webtech@webtech-debian.webtech>
2023-09-18 11:48:15 -0300

    Preguntas tema 1, dificutad media

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+def generar_preguntas_alternativa(tema, dificultad_pregunta, enunciados, alternativas, consejos, respuestas)
+    5.times do |index|
+        AltQuestion.create!(
+          tema_id: tema,
+          enunciado: enunciados[index],
+          alternativa1: alternativas[index][0],
+          alternativa2: alternativas[index][1],
+          alternativa3: alternativas[index][2],
+          alternativa4: alternativas[index][3],
+          tip1: consejos[index][0],
+          tip2: consejos[index][1],
+          tip3: consejos[index][2],
+          tip4: consejos[index][3],
+          respuesta: respuestas[index],
+          dificultad: dificultad_pregunta
+        )
+      end
+end
+
+############# USERS #############
 User.create!(nombre:"Mati",email:"example@example.com",password:"123456",apellido:"munoz")
 User.create!(nombre:"Profesor", email:"profesor@profesor.com", password:"123456", apellido:"Profe")
 
-############# TEMA 1 #############
-Tema.create(materia_actual:"Ley de Gases Ideales")
+############# TEMAS #############
+tema1 = Tema.create(materia_actual:"Ley de Gases Ideales")
+tema2 = Tema.create(materia_actual:"Calidad de mezclas")
+tema3 = Tema.create(materia_actual:"Entalpía y Entropía")
+tema4 = Tema.create(materia_actual:"Calor Latente")
+tema5 = Tema.create(materia_actual:"Tablas de Saturación")
+temas = [tema1, tema2, tema3, tema4, tema5]
 
-#
+############# PREG TEMA 1 #############
+### Fácil ###
 AltQuestion.create!(tema_id:Tema.first().id,enunciado:"¿Qué tipo de información proporciona un diagrama de fases PvT?",
             alternativa1:"Información sobre la densidad de una sustancia.",
             alternativa2:"Información sobre la temperatura de fusión.",
@@ -48,8 +67,72 @@ AltQuestion.create!(tema_id:Tema.first().id,enunciado:"En un diagrama de fases P
             respuesta:'b',
             dificultad:1)
 
-############# TEMA 2 #############
-Tema.create(materia_actual:"Calidad de mezclas")
+AltQuestion.create!(tema_id: Tema.first().id, enunciado:"¿Cuál es la principal variable representada en un diagrama de fases vT (volumen específico vs. temperatura)?", 
+            alternativa1:"Presión", 
+            alternativa2:"Densidad", 
+            alternativa3:"Volumen", 
+            alternativa4:"Temperatura",
+            tip1:"Este tipo de diagrama se centra en cómo cambian las fases en función de la temperatura a presión constante.",
+            tip2:"Este tipo de diagrama se centra en cómo cambian las fases en función de la temperatura a presión constante.",
+            tip3:"Este tipo de diagrama se centra en cómo cambian las fases en función de la temperatura a presión constante.",
+            tip4:"Correcto!",
+            respuesta:"d",
+            dificultad:1)
+
+AltQuestion.create!(tema_id:Tema.first.id, enunciado:"¿Qué información se puede obtener de un diagrama de fases PT (presión vs. temperatura)?", 
+            alternativa1:"Volumen específico", 
+            alternativa2:"Estados de la materia", 
+            alternativa3:"Energía interna", 
+            alternativa4:"Densidad de masa",
+            tip1:"Se centra principalmente en las transiciones de fase entre sólido, líquido y gas en función de la presión y la temperatura.",
+            tip2:"Correcto!",
+            tip3:"La energía interna suele estar relacionada con propiedades termodinámicas que no se representan en un diagrama de fases PT.",
+            tip4:"Un diagrama de fases PT no proporciona información directa sobre la densidad de masa.",
+            respuesta:"b",
+            dificultad:1)
+
+### Media ###
+enunciados_tema1_media = [
+    "En un diagrama de fases PT para una sustancia, ¿qué representa el punto crítico?",
+    "Se tiene un sistema que consiste en una mezcla de vapor de agua y agua líquida en equilibrio en un recipiente cerrado. ¿En qué parte del diagrama de fases PvT se encuentra este sistema?",
+    "En un diagrama de fases vT, ¿cómo se llama la curva que marca el límite entre la región de vapor saturado y la región de vapor sobrecalentado?",
+    "Para una sustancia en un diagrama de fases PT, ¿qué se puede decir sobre un punto que se encuentra por debajo de la curva de coexistencia sólido-líquido?",
+    "En un diagrama de fases Pv, ¿cómo se denomina el proceso de transición directa entre los estados sólido y gaseoso, omitiendo el estado líquido?"
+]
+
+alternativas_tema1_media = [
+    ["El punto en el que todas las fases coexisten en equilibrio.", "El punto de congelación de la sustancia.", "El punto en el que las fases sólida y líquida son indistinguibles.",  "El punto de máxima densidad de la sustancia."], 
+    ["Por encima de la curva de saturación.", "En la curva de coexistencia de fases.", "En la región de vapor saturado.", "En la región de líquido comprimido."],
+    ["Línea crítica.", "Línea de sublimación.", "Línea de saturación.", "Línea de transición."],
+    ["Está en la región de vapor saturado.", "Está en la región de vapor sobrecalentado.", "Está en la región de líquido comprimido.", "Está en un estado metaestable."],
+    ["Evaporación.", "Sublimación.", "Condensación.", "Fusión."]
+]
+
+tips_tema1_media = [
+    ["Correcto!","En este punto, no hay distinción clara entre las fases, y las propiedades de la sustancia se vuelven únicas.","En este punto, no hay distinción clara entre las fases, y las propiedades de la sustancia se vuelven únicas.","En este punto, no hay distinción clara entre las fases, y las propiedades de la sustancia se vuelven únicas."],
+    ["En esta región, tanto el vapor como el líquido coexisten y están en equilibrio.","En esta región, tanto el vapor como el líquido coexisten y están en equilibrio.","Correcto!","En esta región, tanto el vapor como el líquido coexisten y están en equilibrio."],
+    ["Este concepto es muy general para lo que se pide.","Intenta descifrar lo que es sublimación.","Correcto!","Este concepto es muy general para lo que se pide."],
+    ["Podría estar en una fase diferente de lo que se esperaría en equilibrio en esas condiciones específicas.","Piensa en el estado de todos los componentes.","Piensa en el estado presente debajo de la curva.","Correcto!"],
+    ["Vuelve a leer el enunciado, hay una palabra clave que te ayudará.","Correcto!","Realmente se está omitiendo el estado líquido?","Realmente se está omitiendo el estado líquido?"]
+]
+
+respuestas_tema1_media = ["a","c","c","d","b"]
+
+generar_preguntas_alternativa(1, 2, enunciados_tema1_media, alternativas_tema1_media, tips_tema1_media, respuestas_tema1_media)
+
+# AltQuestion.create!(tema_id:, enunciado:, 
+#             alternativa1:, 
+#             alternativa2:, 
+#             alternativa3:, 
+#             alternativa4:,
+#             tip1:,
+#             tip2:,
+#             tip3:,
+#             tip4:,
+#             respuesta:,
+#             dificultad:)
+
+############# PREG TEMA 2 #############
 
 AltQuestion.create!(tema_id: Tema.last.id, enunciado:"¿Qué se entiende por 'calidad' en el contexto de las mezclas?",
             alternativa1:"La cantidad total de sustancias en la mezcla.",
@@ -98,12 +181,3 @@ AltQuestion.create!(tema_id: Tema.last.id, enunciado:"Si se tiene una mezcla en 
             tip4:"En el caso de componentes idénticos en estado de vapor, la calidad es perfectamente determinable y siempre será un valor determinado.",
             respuesta:'b',
             dificultad:1)
-
-############# TEMA 3 #############
-Tema.create(materia_actual:"Entalpía y Entropía")
-
-############# TEMA 4 #############
-Tema.create(materia_actual:"Calor Latente")
-
-############# TEMA 5 #############
-Tema.create(materia_actual:"Tablas de Saturación")
